3b18a30183d676fda0a6fe92e701093b591715de
= <=>
2020-03-24 06:37:11 -0500

    init

---
+++ app/app/AmazonS3Interface.cs
@@ -0,0 +1,55 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class AmazonS3Interface
+    {
+        private static readonly IAmazonS3 _client = new AmazonS3Client(RegionEndpoint.USEast1);
+
+        public async Task<List<string>> GetZippedFileContentsFromS3Bucket(string bucketName, string keyName)
+        {
+            try
+            {
+                var request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+
+                using var response = await _client.GetObjectAsync(request);
+
+                using var responseStream = response.ResponseStream;
+
+                Stream unzippedResponseStream;
+
+                var archive = new ZipArchive(responseStream);
+
+                var entries = new List<string>();
+
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    unzippedResponseStream = entry.Open();
+
+                    using var reader = new StreamReader(unzippedResponseStream);
+
+                    entries.Add(reader.ReadToEnd());
+                }
+
+                return entries;
+            }
+
+            catch (Exception ex)
+            {
+                return null; // TODO
+            }
+        }
+    }
+
+}
---
+++ app/app/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace app
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            var s3Interface = new AmazonS3Interface();
+
+            var zippedFiles = s3Interface.GetZippedFileContentsFromS3Bucket("principalinterview", "alkirempi.zip")
+                .Result;
+
+            // TODO:
+        }
+    }
+}
